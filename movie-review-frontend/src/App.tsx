65d7feab8dbcc9114cc07a1b47d7924c70271690
afsanajamal <afsanajamalmumu@gmail.com>
2024-02-20 17:43:22 +0900

    tailwind support added

--- movie-review-frontend/src/App.tsx
+++ movie-review-frontend/src/App.tsx
@@ -2,7 +2,7 @@
 // import reactLogo from "./assets/react.svg"
 // import viteLogo from "/vite.svg"
 import "./App.css"
-import Home from "./pages/Home"
+// import Home from "./pages/Home"
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -31,7 +31,10 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       */}
-      <Home />
+      <h1 className="text-3xl font-bold underline text-red-600">
+        Simple React Typescript Tailwind Sample
+      </h1>
+      
     </>
   )
 }
